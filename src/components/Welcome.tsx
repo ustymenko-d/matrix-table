d6dcf40249f92c5d0a98424b995e44b7786cab50
ustymenko-d <ustymenko.denys@gmail.com>
2025-09-15 10:04:08 +0300

    feat: conditional rendering of instructions in the welcome section

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,20 +1,27 @@
-const Welcome = () => (
-	<section className='mb-12 px-4 pt-12'>
-		<div className='flex max-w-xl flex-col gap-2'>
-			<h1 className='mb-2 text-4xl font-bold text-foreground'>
-				Welcome to Matrix{'\u00A0'}Table
-			</h1>
+import useFormContext from '@/hooks/useFormContext';
 
-			<p className='text-lg text-foreground'>
-				This application allows you to manage and interact with structured
-				tabular data.
-			</p>
+const Welcome = () => {
+	const { step } = useFormContext();
+	return (
+		<section className='mb-12 px-4 pt-12'>
+			<div className='flex max-w-xl flex-col gap-2'>
+				<h1 className='mb-2 text-4xl font-bold text-foreground'>
+					Welcome to Matrix{'\u00A0'}Table
+				</h1>
 
-			<p className='text-base text-muted-foreground'>
-				To get started, please fill out the form below.
-			</p>
-		</div>
-	</section>
-);
+				<p className='text-lg text-foreground'>
+					This application allows you to manage and interact with structured
+					tabular data.
+				</p>
+
+				{step !== 'Done' && (
+					<p className='text-base text-muted-foreground'>
+						To get started, please fill out the form below.
+					</p>
+				)}
+			</div>
+		</section>
+	);
+};
 
 export default Welcome;
